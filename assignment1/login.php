bf5bf600b1818a785a20c2d71ae7613584cea29f
yavuzkoca <ykoca0808@gmail.com>
2017-10-20 13:39:44 +0000

    assignment1

---
+++ assignment1/login.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type"; content="text/html; charset=utf-8"; />
+<title>YAVUZ KOCA</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Description" content="Şiir, Duygu, Yavuz, GURUR"/>
+<meta name="Keywords" content="XHTML, HTML, HTML5, CSS"/>
+<meta http-equiv="content-language" content="tr"/>
+<meta name="revisit-after" content="1 days"/> 
+<meta name="robots" content="index,follow"/>
+<link rel="stylesheet" href="bootstrap.css" media="all" type="text/css" />
+<link rel="stylesheet" href="css.css" media="all" type="text/css" />
+<link rel="stylesheet" href="style.css" media="all" type="text/css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<style type="text/css"></style>
+<script type="text/javascript">
+            function show(){
+				var e = document.getElementById("show_whoami");
+				e.hidden = !e.hidden;}
+			
+			$(function(){
+               $('#show_register').click(function(){
+                  $('.login_form').hide(); 
+                  $('.register_form').show();
+                  return false;
+               }); 
+              
+        
+               $('#show_login').click(function(){
+                   $('.login_form').show();
+                   $('.register_form').hide();
+               })
+            });
+        </script>
+		<style>
+		.website ul li{
+		list-style-type:circle;}
+		div h4{
+		padding-top:15px; 	
+		}
+		footer{
+		text-align:center;
+		font-size:30px;
+		padding:20px;
+		line-height:40px;
+		font-family:"times new roman";
+		background-color:#99FFCC;
+		margin-left:-470px;
+		margin-right:-449.5px;
+		border-top: 4px solid black;
+		}
+		footer a{
+		color:#000;}
+		</style>
+		
+</head>
+
+<body>
+	<div class="discerceve">
+    	
+        
+        	<div class="solbosluk">
+        	</div>
+    	
+        
+        
+        	<div class="anasayfa">
+            		
+                    
+                    
+                            <div class="ustbosluk">
+                                            <!--en üst taraf küçük boşluk -->
+                                            
+                                                
+                                                
+                                             <div class="ikonlar">	
+                                             		
+                                                    
+                                                    <table>
+                                                      	<tr>
+                                                         	<td>
+                                                            	<a href="https://www.facebook.com/yavuz.koca.9028">	
+                                                                    <img src="img/f-t-i.fw_r1_c1.gif" width=40px; style="padding-left:10px;" alt="fb" />
+                                                         		</a></td>
+                                                            <td>
+                                                            	<a href="https://twitter.com/GhostBoy08">	
+                                                                    <img src="img/f-t-i.fw_r1_c2.gif" width=40px; alt="tw" />
+                                                      			</a></td>
+                                                            <td>
+                                                            	<a href="https://www.instagram.com/y.koca08/">
+                                                                	<img src="img/f-t-i.fw_r1_c3.gif" width=40px; alt="in" />
+                                                         		</a></td>
+                                                         </tr>
+                                                      	
+                                                   </table>	       
+                                             
+                                             
+                                             
+                                             </div>
+                                            
+                                            
+                                            
+                                            
+                                           
+                                            <div class="uye-girisi" style="padding-top:10px; padding-bottom:-20px; margin-bottom:-10px;">
+                                                 <a href="login.php" class="four">
+													 <!-- Indicates a successful or positive action -->
+													<button type="button" class="btn btn-success">Log in</button>
+												 </a>
+                                           </div>
+                                            
+                                            
+                                            
+                         </div> <!--Ustbosluk-->
+                            
+                            
+                            
+                            
+                         <ul id=q>     
+                                
+                                <li><a href="lessons.html">LESSONS</a>
+                                    <ul>
+                                    	<li><a href="">BIL 103</a></li>
+                                        <li><a href="">BLG 231</a></li>
+                                        <li><a href="">EHB 222</a></li>
+                                    </ul>
+                                </li>
+                                
+                                
+                                <li><a href="https://drive.google.com/open?id=0BwA8kKHSLiM4QkY3V2Q5bEJEclU">PHOTOS</a>
+                                    <ul>
+                                    	<li><a href="https://drive.google.com/open?id=0BwA8kKHSLiM4QXVUWDVQUzBkblU">Istanbul</a></li>
+                                        <li><a href="https://drive.google.com/open?id=0BwA8kKHSLiM4R05lZHAzQlJTZDQ">Artvin</a></li>
+                                        <li><a href="https://drive.google.com/open?id=0BwA8kKHSLiM4TnZNX3UtMGxfNWc">Ankara</a></li>
+                                    </ul>
+                                </li>
+                                
+                                <a href="index.html" >
+                                <li class="baslik-2">
+                                	
+                                </li>
+                                </a>
+                                <li><a href="advices.html">Advices</a>
+                                    <ul>
+                                    	<li><a href="movies.html">Movies</a></li>
+                                        <li><a href="tv_series.html">Tv Series</a></li>
+                                        <li><a href="games.html">Games</a></li>
+                                    </ul>
+                                </li>
+                                
+                                
+                                
+                                <li><a href="#" onclick="show()">WHOAMI</a>
+                                    
+                                </li>
+                                
+                            
+                        </ul><!--menu-->
+                    
+                        <div class="altanasayfa">
+                                <div class="altanasayfaustbosluk">
+                                </div>
+                                
+                                
+                                
+                                <div class="altanasayfasol"><font style="color:#fff">a</font>
+                                </div>
+                                <div class="altanasayfaorta"><!--merkezi yer-->
+                                	<form id="show_whoami" hidden>
+                                    	<div style="border:dotted 3px #FF0000; padding:8px 0px 8px 10px; background-color:#000; color:#CF0; font-family:'Lucida Console', Monaco, monospace;">
+                                        YAVUZ KOCA<br/>
+										Istanbul Technical University Computer Engineering<br/>
+										Phone: 0554 845 65 36<br/>
+										E-Mail: deathghost6608@gmail.com<br/>
+                                         </div>
+                                	</form>
+									<div id="mainWrapper">
+										<div class="navbar">
+											<div class="navbar-inner">
+												<div class="container">
+													<a class="brand" href="#">
+														Member Form
+													</a> 
+											
+												</div> <!--end container-->
+											</div> <!--end navbar-inner-->   
+										</div> <!--end navbar-->
+										<div id="content">
+											
+											
+											<div id="" class="login_form">
+												<div id="form">
+													<h2> Login Here</h2>
+													<form method="post" action="login.php">
+													   <div class="form_elements">
+																<label for="Username">Username</label>
+																<input type="text" name="login_username" id="username"/>
+														</div><!--end form_elements-->
+
+														<div class="form_elements">
+															<label for="Username">Password</label>
+															<input type="password" name="login_password" id="password"/>
+														</div><!--end form_elements-->
+
+														<div class="form_elements">
+															<input type="submit" name="login" id="login" class="btn btn-success" value="Login"/>
+														</div><!--end form_elements-->
+														<br/>
+														<a href="#" id="show_register"> Don't have an account</a>
+														
+													</form>
+												</div> <!--end form-->    
+												
+											</div><!--end login_form-->
+											<br/>
+											<div class="register_form">
+
+												<div id="form">
+													<form method="post" action="login.php">
+
+														<h2>Register Here</h2>
+														<div class="form_elements">
+															<label for="Username">Username</label>
+															<input type="text" name="username" id="username"/>
+														</div><!--end form_elements-->
+
+														<div class="form_elements">
+															<label for="Username">Email</label>
+															<input type="text" name="email" id="email"/>
+														</div><!--end form_elements-->
+
+														<div class="form_elements">
+															<label for="Username">Password</label>
+															<input type="password" name="password" id="password"/>
+														</div><!--end form_elements-->
+
+														 <div class="form_elements">
+															<label for="Username">Re-Password</label>
+															<input type="password" name="repassword" id="repassword"/>
+														</div><!--end form_elements-->
+
+														<div class="form_elements">
+															<input type="submit" name="register" id="register" class="btn btn-success"/>
+														</div><!--end form_elements-->
+
+													</form>
+													<br/>
+														<a href="#" id="show_login"> Already have an account</a>
+												</div><!--end form-->
+											</div><!--end register_form-->
+											
+										</div> <!--end content-->
+										
+									</div> <!--end mainWrapper-->
+									<footer><strong><a href="index.html">Yavuz KOCA</a></strong><br/>
+											Istanbul Technical University<br/>
+										<div class="a">This site has been created with support of <a href="http://pythonanywhere.com">pythonanywhere.com</a>
+										</div>
+									</footer>
+                                	
+                                </div>
+                                <div class="altanasayfasag">
+                                </div>
+                       </div><!--altanasayfa-->
+        			   <div class="clear">
+                       </div>
+       	    
+            
+            </div><!--anasayfa-->
+    	
+        
+        
+        
+            <div class="sagbosluk">
+            </div>
+        
+        
+    </div>
+
+</body>
+</html>
